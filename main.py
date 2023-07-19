89c74995da527d27208b995d973285b47d78772b
Alessandro Baiocchi <alessandro.baiocchi@uniroma1.it>
2023-07-19 17:48:57 +0200

    Added dataset support for ModelNet40 Added main Added hydra config

---
+++ main.py
@@ -0,0 +1,75 @@
+import hydra
+from omegaconf import DictConfig
+import torch
+import wandb
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from datasets.ModelNet40Ply2048 import ModelNet40Ply2048DataModule
+from model import Adapt_classf_pl
+import os
+
+
+os.environ["WANDB_API_KEY"] = "04a5d6fba030b76e5b620f5bd6509cf7dffebb8b"
+
+def train(cfg, train_loader, test_loader):
+
+    device = "cuda" if cfg.cuda else "cpu"
+    if cfg.model.name == "Adapt_classf":
+        model = Adapt_classf_pl(cfg.model.embed_dim, cfg.n_points, cfg.n_classes, cfg.model.n_blocks, cfg.model.drop_loc, cfg.model.drop_rate, cfg.model.groups, cfg.train.lr, cfg.train.weight_decay)
+    else:
+        raise Exception("Model not supported")
+    
+    if cfg.wandb:
+        wandb_logger = WandbLogger(name=cfg.experiment.name, project=cfg.experiment.project)
+        wandb_logger.watch(model)
+        wandb_logger.log_hyperparams(cfg)
+        wandb_logger.log_hyperparams(model.hparams)
+
+    trainer = pl.Trainer(max_epochs=cfg.train.epochs, accelerator=device, logger=[wandb_logger] if cfg.wandb else None, devices=1, gradient_clip_val=2)
+    trainer.fit(model, train_loader, test_loader)
+
+
+
+    return None
+
+def test(cfg, test_loader):
+    raise NotImplementedError
+
+def visualize(cfg):
+    raise NotImplementedError
+
+@hydra.main(config_path=".", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    pl.seed_everything(cfg.experiment.seed)
+    if cfg.wandb:
+        wandb.login()
+        wandb.init(config=cfg)
+
+    cfg.cuda = cfg.cuda and torch.cuda.is_available()
+    if cfg.cuda:
+        print(
+            'Using GPU : ' + str(torch.cuda.current_device()) + ' from ' + str(torch.cuda.device_count()) + ' devices')
+        torch.cuda.manual_seed(cfg.experiment.seed)
+    else:
+        print('Using CPU')
+
+    if cfg.experiment.dataset == "ModelNet40":
+        dataset = ModelNet40Ply2048DataModule(batch_size=cfg.train.batch_size)
+    else:
+        raise Exception("Dataset not supported")
+    dataset.setup()
+    train_loader = dataset.train_dataloader()
+    test_loader = dataset.val_dataloader()
+    cfg.n_classes = dataset.num_classes
+    cfg.n_points = dataset.num_points
+
+    if not cfg.eval:
+        train(cfg, train_loader, test_loader)
+    else:
+        if not cfg.visualize_pc:
+            test(cfg, test_loader)
+        else:
+            visualize(cfg)
+
+if __name__ == "__main__":
+    main()
--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from einops.layers.torch import Reduce
 from pytorch3d.ops import knn_points
 from util import batch_index_select, channel_shuffle
-
+import pytorch_lightning as pl
 
 #ARPE: Absolute Relative Position Encoding
 class ARPE(nn.Module):
@@ -56,22 +56,20 @@ class GSA(nn.Module):
         B, N, C = x.shape
 
         xin = x # B, N, C
-
         #grouped_x = x.reshape(B, N, C//self.groups, self.groups) # B, N, C//groups, groups
-
+        
         #Si può vettorizzare?
         x_g =[]
         for i in range(self.groups):
             x = self.linears[i](xin[:,:,i*self.cg:(i+1)*self.cg]) # B, N, C//groups
-            x = F.scaled_dot_product_attention(x,x,F.elu(x),mask=mask)
+            x = F.scaled_dot_product_attention(x,x,F.elu(x),attn_mask=mask)
             x_g.append(x)
         x = torch.cat(x_g, dim=-1) # B, N, C
 
         x = self.gn((channel_shuffle(x, self.groups) + xin).transpose(1,2)).transpose(1,2) # B, N, C
 
         return x
-    
-    
+       
 #GSS: Gumbel Softmax Sampling TODO  
 class GSS(nn.Module):
     def __init__(self, in_ch) -> None:
@@ -113,49 +111,51 @@ class DropPredictor(nn.Module):
         return self.out_conv(x)
     
 #Classification head
-class Classifier(nn.Module):
+class Classf_head(nn.Module):
     def __init__(self, in_channels, n_classes) -> None:
         super().__init__()
 
         self.in_channels = in_channels
         self.n_classes = n_classes
-        lin1 = nn.Linear(in_channels, in_channels//2)
-        lin2 = nn.Linear(in_channels//2, n_classes)
-        bn1 = nn.BatchNorm1d(in_channels//2)
-        dp = nn.Dropout(0.5)
-        self.classifier = nn.Sequential(lin1, bn1, nn.GELU(), dp, lin2)
+        self.lin1 = nn.Linear(in_channels, in_channels//2)
+        self.lin2 = nn.Linear(in_channels//2, n_classes)
+        self.bn1 = nn.BatchNorm1d(in_channels//2)
+        self.dp = nn.Dropout(0.5)
 
     def forward(self, x):
-        return self.classifier(x)
+        
+        x = F.gelu(self.bn1(self.lin1(x)))
+        x = self.lin2(self.dp(x))
+        return x
 
-#Whole model
+#Transformer model
 class AdaPT(nn.Module):
-    def __init__(self, embed_dim, n_classes, n_blocks = 3, drop_loc=[], drop_target=[]) -> None:
+    def __init__(self, embed_dim, n_points, n_blocks = 3, drop_loc=[], drop_target=[], groups=1) -> None:
         super().__init__()
 
         self.embed_dim = embed_dim
+        self.n_points = n_points
         self.n_blocks = n_blocks
         self.drop_loc = drop_loc
         self.drop_target = drop_target
+        self.groups = groups
 
-        self.arpe = ARPE()
-        self.blocks = nn.ModuleList([GSA(channels=self.embed_dim) for _ in range(self.n_blocks)])
+        self.arpe = ARPE(in_channels=3, out_channels=self.embed_dim, npoints=self.n_points)
+        self.blocks = nn.ModuleList([GSA(channels=self.embed_dim, groups=self.groups) for _ in range(self.n_blocks)])
         self.predictors = nn.ModuleList([DropPredictor(self.embed_dim) for _ in range(len(self.drop_loc))])
-        self.classifier = Classifier(self.embed_dim, self.n_classes)
         
 
     def forward(self, x, drop_temp=1.0):
 
         B, N, C = x.shape
-
         x = self.arpe(x)
-        
         prev_decision = torch.ones(B, N, 1, dtype=x.dtype, device=x.device)
+        mask = None
         p = 0
 
         for i in range(self.n_blocks):
             if i in self.drop_loc:
-                pred_score = self.score_predictor[p](x, prev_decision)
+                pred_score = self.predictors[p](x, prev_decision)
                 p+=1
                 # Slow warmup
                 keepall = torch.cat((torch.zeros_like(pred_score[:,:,0:1]), torch.ones_like(pred_score[:,:,1:2])),2) 
@@ -164,9 +164,8 @@ class AdaPT(nn.Module):
                     pred_score = torch.log(pred_score + 1e-8)
                     decision = F.gumbel_softmax(pred_score, tau=1.0, hard=True, dim=-1)[:,:,1:2]*prev_decision
                     prev_decision = decision
-                    mask = (decision*decision.transpose(1,2)).bool()
-
-
+                    mask = (decision*decision.transpose(1,2) - torch.diag_embed(decision.squeeze(-1)) + torch.eye(N).repeat(B,1,1)).bool()
+                    
                 else:
                     score = pred_score[:,:,1]
                     num_keep_tokens = int((1-self.args.train.drop_ratio[p]) * (N))
@@ -176,8 +175,59 @@ class AdaPT(nn.Module):
                     mask = None
 
             x = self.blocks[i](x, mask=mask)
+            
+        return x, decision
+    
+#Classifier model
+class Adapt_classf(nn.Module):
+    def __init__(self,embed_dim, n_points, n_classes, n_blocks = 3, drop_loc=[], drop_target=[], groups=1):
+        super().__init__() 
 
+        self.classifier = Classf_head(embed_dim, n_classes)
+        self.adapt = AdaPT(embed_dim, n_points, n_blocks, drop_loc, drop_target, groups)
+
+    def forward(self, x, drop_temp=1.0):
+        x, decision = self.adapt(x, drop_temp)
+        x = torch.sum(x * decision, dim=1) / (torch.sum(decision, dim=1) + 1e-20)
         x = self.classifier(x)
         return x
-    
+
+#Pytorch_lightning wrapper
+class Adapt_classf_pl(pl.LightningModule):
+    def __init__(self, embed_dim, n_points, n_classes, n_blocks = 3, drop_loc=[], drop_target=[], groups=1, lr=1e-3, weight_decay=1e-4):
+        super().__init__() 
+
+        self.save_hyperparameters()
+        self.model = Adapt_classf(embed_dim, n_points, n_classes, n_blocks, drop_loc, drop_target, groups)
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.loss = nn.CrossEntropyLoss()
+
+    def forward(self, x, drop_temp=1.0):
+        return self.model(x, drop_temp)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = self.loss(y_hat, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = self.loss(y_hat, y)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        return optimizer
+
+    def predict(self, x, drop_temp=1.0):
+        return self.model(x, drop_temp)
+
+
+
+
 
